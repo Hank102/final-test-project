1f2bf66429aee2b438b55d7a2054216308161902
Hank102 <80513180+Hank102@users.noreply.github.com>
2021-06-25 15:01:35 +0300

    creating pages

--- test_main_page.py
+++ test_main_page.py
@@ -4,6 +4,3 @@ def test_guest_can_go_to_login_page(browser):
     browser.get(link)
     go_to_login_page(browser)
 
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
