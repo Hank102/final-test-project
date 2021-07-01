26fe3bc40b820260f12127754c661035b0f85326
Hank102 <80513180+Hank102@users.noreply.github.com>
2021-07-01 11:30:20 +0300

    finishing touches

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -4,13 +4,13 @@ from .locators import BasketPageLocators
 
 
 class BasketPage(BasePage):
-    def can_go_to_basket_page(self):
+    def guest_can_go_to_basket_page(self):
         button = self.browser.find_element(*BasePageLocators.BASKET_BUTTON)
         button.click()
 
-    def cant_see_product_in_basket(self):
+    def guest_cant_see_product_in_basket(self):
         assert self.is_not_element_present(*BasketPageLocators.PRODUCT_IN_BASKET), \
             "Product in basket is presented, but should not be"
 
-    def can_see_empty_basket(self):
+    def guest_can_see_empty_basket(self):
         assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET)
